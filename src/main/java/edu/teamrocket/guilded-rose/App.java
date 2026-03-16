f3b7ea32efa46e0b0194422a207e1764c9651537
FernanPC18 <231477850+FernanPC18@users.noreply.github.com>
2026-03-16 13:14:37 +0100

    build(guilded-rose): Configurar POM del proyecto Maven

---
+++ src/main/java/edu/teamrocket/guilded-rose/App.java
@@ -0,0 +1,10 @@
+package edu.teamrocket.guilded-rose;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
